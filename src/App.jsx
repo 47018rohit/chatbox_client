d8149699eff653778bc4b088d458b7f2bf3f0e81
hakkanoodles <47018rohit@gmail.com>
2023-07-04 17:38:03 +0530

    able to emit to specific client

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,36 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import { socket } from './socket/socket'
 
 function App() {
+  const [sendMessage, setSendMessage] = useState("")
+  const [recieveMessage, setRecieveMessage] = useState("")
+  const [roomId, setRoomId] = useState('')
 
-  socket.on("connect", () => {
-    // console.log(socket.id)
-  })
 
-  socket.on("chat_message", ()=>{
-    socket.emit("hello from client")
-  })
+  useEffect(() => {
+    socket.on("recieve_message", message => {
+      setRecieveMessage(message)
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [socket])
 
-  socket.on("disconnect" , (socket)=>{
-    console.log(socket.id)
-  })
+  const sendMessageClick = () => {
+    socket.emit("send_message",{ room:roomId, message:sendMessage})
+  }
+  const joinRoom = () => {
+    if(roomId !== "")  socket.emit("join_room", roomId)
+  }
 
   return (
-    <>
-      hello world
-    </>
+    <div>
+      <input placeholder='Room ID..' onChange={e => setRoomId(e.target.value)} />
+      <button onClick={joinRoom}>Join Room</button>
+      <input placeholder='Message...' onChange={e => setSendMessage(e.target.value)} />
+      <button onClick={sendMessageClick}>Send Message</button>
+
+      <h3>{recieveMessage}</h3>
+    </div>
   )
 }
 
